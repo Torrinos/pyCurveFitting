aa8cb27a73c0112eaaed37bf1d98e841a8285ba2
Torrinos <Torrinos@torrinos.com>
2014-01-03 11:35:41 -0700

    Cleaning up

--- Source/MyParser.py
+++ Source/MyParser.py
@@ -1,92 +1,78 @@
 # -*- coding: utf-8 -*-
 """
 Parser for mathematical expressions written in F(X) form
+Class EQParser is used to par
 
 Created on Tue Dec 31 11:21:35 2013
-
 @author: OMedvedev
 """
 
-"""
-#sympy version - scalability issues with variables list
-#def _parser(expr):
-    from sympy import symbols, lambdify,var
-    #from sympy.abc import x,y,a,b,c,d,e,f,g,h
-    a,b,c,d = symbols('a:d',real=True)
-    e,f,g,h = symbols('e:h',real=True)
-    i,j,k,l = symbols('i:l',real=True)    
-    x,y,z = symbols('x:z', real=True)
+class EQParser():
+    def __init__(self):
+        self._varStack = []
+        self._pattern = self._defPattern()
     
-    A,B,C,D = symbols('A:D',real=True)
-    E,F,G,H = symbols('E:H',real=True)
-    I,J,K,L = symbols('I:L',real=True)    
-    X,Y,Z = symbols('x:z', real=True)
-    f=lambdify([a,b,c,d,x],expr,'numpy')
-    return f
-"""
-
-import pyparsing as pp
-from numpy import *
-from scitools.StringFunction import StringFunction
-
-global varStack
-varStack = []
-
-def addVar(varString, tokens):
-    varStack.append( tokens[0] )
-
-def defPattern():
-    pattern = None        
-    #defining sub-variables for tokens
-    pi    = pp.CaselessLiteral( "$PI" )
-    e     = pp.CaselessLiteral( "$E" )
-    plus  = pp.Literal( "+" )
-    minus = pp.Literal( "-" )
-    mult  = pp.Literal( "*" )
-    div   = pp.Literal( "/" )
-    lpar  = pp.Literal( "(" ).suppress()
-    rpar  = pp.Literal( ")" ).suppress()
-    point = pp.Literal( "." )
-
-    #defining variables     
-    fnumber = pp.Combine( pp.Word( "+-"+pp.nums, pp.nums ) + 
-                       pp.Optional( point + pp.Optional( pp.Word( pp.nums ) ) ) +
-                       pp.Optional( e + pp.Word( "+-"+pp.nums, pp.nums ) ) )
-    #ident = pp.Word(pp.alphas, pp.alphas+pp.nums+"_$")
-    funct = pp.oneOf("sin cos tan log exp log",caseless=True)
-    varia = pp.Word( pp.srange("[a-zA-Z_]"),max=1)
-    addop  = plus | minus
-    mulop = mult | div
-    expop = pp.Literal( "**" )
-
-    #defining pattern itself
-    expr = pp.Forward()
-    atom = (pp.Optional("-") + ( pi | e | varia.addParseAction(addVar) | fnumber | funct + lpar + expr + rpar ) | ( lpar + expr.suppress() + rpar )) 
-    # by defining exponentiation as "atom [ ^ factor ]..." instead of "atom [ ^ atom ]...", we get right-to-left exponents, instead of left-to-righ
-    # that is, 2^3^2 = 2^(3^2), not (2^3)^2.
-    factor = pp.Forward()
-    factor << atom + pp.ZeroOrMore( ( expop + factor ))
+    def _defPattern(self):
+        import pyparsing as pp
+        pattern = None        
+        #defining sub-variables for tokens
+        pi    = pp.CaselessLiteral( "$PI" )
+        e     = pp.CaselessLiteral( "$E" )
+        plus  = pp.Literal( "+" )
+        minus = pp.Literal( "-" )
+        mult  = pp.Literal( "*" )
+        div   = pp.Literal( "/" )
+        lpar  = pp.Literal( "(" ).suppress()
+        rpar  = pp.Literal( ")" ).suppress()
+        point = pp.Literal( "." )
     
-    term = factor + pp.ZeroOrMore( ( mulop + factor ))
-    expr << term + pp.ZeroOrMore( ( addop + term ))
-    pattern = expr
-    return pattern
-
-def getVars(testString):
-    defPattern().parseString(testString)
-    return sorted(set(varStack))
-
-def test(myFStr):
-    myVars=getVars(myFStr)
-    Fun=StringFunction(myFStr,independent_variables=myVars,globals=globals())
+        #defining variables     
+        fnumber = pp.Combine( pp.Word( "+-"+pp.nums, pp.nums ) + 
+                           pp.Optional( point + pp.Optional( pp.Word( pp.nums ) ) ) +
+                           pp.Optional( e + pp.Word( "+-"+pp.nums, pp.nums ) ) )
+        #ident = pp.Word(pp.alphas, pp.alphas+pp.nums+"_$")
+        funct = pp.oneOf("sin cos tan log exp log sqrt",caseless=True)
+        varia = pp.Word( pp.srange("[a-zA-Z_]"),max=1)
+        addop  = plus | minus
+        mulop = mult | div
+        expop = pp.Literal( "**" )
     
-    x=linspace(0,100)    
-    
-    Par=zeros(len(myVars)-1)
-    for i in range(len(myVars)-2):
-        Par[i]=i+0.2
-    return (Fun(*Par,x=x),2+4*cos(x)+sin(x))
-    #return Par[0]+Par[1]*x**2
+        #defining pattern itself
+        expr = pp.Forward()
+        atom = (pp.Optional("-") + ( pi | e | varia.addParseAction(self._addVar2Stack) | 
+            fnumber | funct + lpar + expr + rpar ) | ( lpar + expr.suppress() + rpar )) 
+        # by defining exponentiation as "atom [ ^ factor ]..." instead of "atom [ ^ atom ]...", 
+        # we get right-to-left exponents, instead of left-to-righ
+        # that is, 2^3^2 = 2^(3^2), not (2^3)^2.
+        factor = pp.Forward()
+        factor << atom + pp.ZeroOrMore( ( expop + factor ))
+        
+        term = factor + pp.ZeroOrMore( ( mulop + factor ))
+        expr << term + pp.ZeroOrMore( ( addop + term ))
+        pattern = expr
+        return pattern
+        
+    def _addVar2Stack(self,varString,tokens):
+        self._varStack.append( tokens[0])
+        
+    def getVars(self,testString):
+        self._pattern.parseString(testString)
+        return sorted(set(self._varStack))
+        
+#Test Section, please ignore
+if __name__ == "__main__":
+    from numpy import *
+    from scitools.StringFunction import StringFunction
 
-#print getVars("a+b*sin(c*x)")
-#print test("a+b*x+c*x**2+cos(x)")
+    def test(myFStr,Params,X):
+
+        myEQParser = EQParser()
+        myVars=myEQParser.getVars(myFStr)
+        Fun=StringFunction(myFStr,independent_variables=myVars,globals=globals())
+        return Fun(*Params,x=X)
+        
+    myEQParser = EQParser()
+
+    print test("a+b*cos(c*x)+sin(x)",([2,4,5.1]),1.21)-(2+4*cos(5.1*1.21)+sin(1.21))
+    print test("a+b**c**d**x",([1.1,2.1,.12,4.2]),1.298)-(1.1+2.1**0.12**4.2**1.298)
+    print test("a+b**cot",([1.1,2.1,.12,4.2]),1.298)-(1.1+2.1**0.12**4.2**1.298)
--- Source/PyCurveFitting.py
+++ Source/PyCurveFitting.py
@@ -5,13 +5,16 @@ Created on Thu Jan 02 11:43:01 2014
 @author: OMedvedev
 """
 
-import MyParser as mp
+from MyParser import *
 import lmfit as lm
 import numpy as np
 #numpy globals are required for StringFunction to work with arrays
-from numpy import sin, cos, exp, tan, log
+from numpy import sin, cos, exp, tan, log, sqrt
 from scitools.StringFunction import StringFunction
 from operator import itemgetter
+
+#initiate a parser class
+myParser=EQParser()
 
 def BasicStats(yvector):
     ave=np.mean(yvector)
@@ -20,22 +23,26 @@ def BasicStats(yvector):
         res += (i-ave)**2
     return res
 
+"""
 #read data to be fitted
 with open('../Data/Temperatures.dat','r') as datafile:
     tt=np.genfromtxt(datafile,names=True,skip_header=17,skip_footer=2)
     tx=tt['YEAR']
     ty=tt['JUN']
+"""
 
 #Synthetic dataset
 tx = np.linspace(0, 10)
-ty = (5. + 0.2*np.sin(2 * tx - 0.1) + \
-    np.random.normal(size=len(tx), scale=0.3))
+#ty = (5. + 0.2*np.sin(2 * tx - 0.1) + \
+#    0.1*np.random.normal(size=len(tx), scale=0.3))
+ty = 0.1*sqrt(0.2*tx**3)
 SStot=BasicStats(ty)
     
 class CReport:
-    def __init__(self,String,Quality,FitResult,FitReport):
-        self.Quality = Quality
-        self.String = String
+    def __init__(self,Function,R2,Chi2,FitResult,FitReport):
+        self.R2 = R2
+        self.Chi2 = Chi2
+        self.Function = Function
         self.Result = FitResult
         self.Report = FitReport
 
@@ -43,7 +50,12 @@ def Fitter(FString):
     result=None
     report=None
     
-    myVars=mp.getVars(FString)
+    try:    
+        myVars=myParser.getVars(FString)
+    except:
+        print ("Can't parse equation %s" %FString +"\n")
+        pass
+    
     myFunc=StringFunction(FString,independent_variables=myVars,\
         globals=globals())
     
@@ -51,7 +63,7 @@ def Fitter(FString):
     #initial values are defined here
     params = lm.Parameters()
     for i in range(len(myVars)-1):
-        params.add(myVars[i],value=1)
+        params.add(myVars[i],value=0.1)
       
     # define objective function: returns the array to be minimized
     def ObjFunc(params, x, data):
@@ -66,9 +78,11 @@ def Fitter(FString):
     
     result = lm.minimize(ObjFunc, params, args=(tx,ty),method='leastsq')
     report = lm.fit_report(params)
-    #quality = result.redchi
-    quality = 1-result.chisqr/SStot
-    return CReport(FString,quality,result,report)
+    
+    #Calculate both quality factors - R2 and ReducedChi2.
+    R2 = 1-result.chisqr/SStot
+    RChi2 = result.redchi
+    return CReport(FString,R2,RChi2,result,report)
 
 #read and create function to be used
 with open('../Data/ShortList.dat','r') as funcfile:
@@ -77,35 +91,35 @@ with open('../Data/ShortList.dat','r') as funcfile:
         try:
             Res=Fitter(line.rstrip())
         except:
-            Res=CReport(line,None,None,None)            
+            Res=CReport(line,None,None,None,None)        
             pass
-        myRes.append([Res.Quality,Res])
+        #modify this line if you want sorting based on R2 or Chi2        
+        myRes.append([Res.R2,Res])
         
 
 #sort results - smallest will be first in the list
 #if using R2 need reverse=True
-#if using Chi2 need reverse=False
+#if using RedChi2 need reverse=False, although too smal RedChi2 may indicate overfitting
 myRes.sort(key=itemgetter(0),reverse=True)
-print myRes
+
+with open('../Data/Result.dat','w') as resfile:
+    for i in range(3):
+        resfile.write("Result %i" %i + "\n")
+        resfile.write(myRes[i][1].Report)
+        resfile.write("\n")
 
 #try to plot best three results
 try:
     import matplotlib.pyplot as plt
+    import matplotlib as mpl                                
     colors = ('b','g','r','c','m','y','k','w',)
-    matplotlib.rcParams.update({'font.size': 6})
+    mpl.rcParams.update({'font.size': 6})
     plt.plot(tx, ty, 'k+')
     for i in range(3):
-        mylabel=myRes[i][1].String + " -> R2= %.4f" % myRes[i][0]
+        mylabel=myRes[i][1].Function + " -> R2= %.4f" % myRes[i][0]
         plt.plot(tx, ty+myRes[i][1].Result.residual,colors[i],\
             label=mylabel)
         plt.legend()
         plt.show()
 except:
     pass
-
-#with open('../Data/debug','w') as outfile:
-#    np.savetxt(outfile, tx)
-#    outfile.write(myRes[0][1].String)    
-#    np.savetxt(outfile, ty)
-#    outfile.write(str(myRes[0][0]))
-#    np.savetxt(outfile,ty+myRes[i][1].Result.residual)    
